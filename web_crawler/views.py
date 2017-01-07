fb3308d8cff9918c6b248205cbba00e78926f0eb
winzett <winzett0@gmail.com>
2017-01-07 03:00:00 -0500

    add html page that has a crawling function

--- web_crawler/views.py
+++ web_crawler/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
 # Create your views here.
 def index(request):
     return render(request, 'web_crawler/index.html')
 
-
-
+def crawling(request):
+  return render(request,'web_crawler/crawling_result.html')
